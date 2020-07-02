3f53dc4315d029adabcde481a4d157c986a023d7
alonganot10 <alonganot@gmail.com>
2020-07-02 21:19:54 +0300

    hackathon #1

--- Hackathon #1/script.js
+++ Hackathon #1/script.js
@@ -1,147 +1,208 @@
-let board = document.getElementById("wrapper");
-for (let x = 1; x < 9; x+=2) { // creates 4 sets of 2 rows
-	for (let y = 1; y < 9; y++) { // creats first row
-		let block = document.createElement("div");
-		if (y % 2 != 0) {
-			block.className = 'white';
-			block.id = `${x} ${y}`;
+//################################################
+// deck creation
+
+function create_deck() {
+	let clover_deck = [];
+	for (let i = 0; i < 13; i++) {
+		clover_deck.push([`cards/C_${i+1}.jpg`, i+1]);
+	}
+	let diamond_deck = [];
+	for (let i = 0; i < 13; i++) {
+		diamond_deck.push([`cards/D_${i+1}.jpg`, i+1]);
+	}
+	let spade_deck = [];
+	for (let i = 0; i < 13; i++) {
+		spade_deck.push([`cards/S_${i+1}.jpg`, i+1]);
+	}
+	let heart_deck = [];
+	for (let i = 0; i < 13; i++) {
+		heart_deck.push([`cards/H_${i+1}.jpg`, i+1]);
+	}
+
+	return [clover_deck, diamond_deck, spade_deck, heart_deck];
+}
+
+//################################################
+// shuffle the deck
+
+function shuffle_deck(deck) {
+	let shuffle = [];
+	for (let i = 0; i < 52; i++) {
+		let random_deck = Math.floor(Math.random()*(deck.length));
+		if (deck[random_deck].length == 0) {
+			deck.splice(random_deck, 1);
 		} else {
-			block.className = 'black';
-			block.id = `${x} ${y}`;
+			let random_card = Math.floor(Math.random()*(deck[random_deck].length));
+			shuffle.push(deck[random_deck][random_card]);
 		}
-		board.appendChild(block);
 	}
-	for (let y = 1; y < 9; y++) { // creates second row
-		let block = document.createElement("div");
-		if (y % 2 == 0) {
-			block.className = 'white';
-			block.id = `${x+1} ${y}`;
+	for (let i = shuffle.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * (i + 1));
+		let temp = shuffle[i];
+		shuffle[i] = shuffle[j];
+		shuffle[j] = temp;
+	}
+	return shuffle;
+}
+
+//################################################
+// hit - add another card to the players hand
+
+function hit() {
+
+	let card0 = deck.pop();
+	choose_ace(card0);
+	player_cards.push(card0);
+	let player_side = document.getElementById("player");
+	let card = document.createElement("img");
+	card.src = player_cards[player_cards.length-1][0];
+	player_side.appendChild(card);
+
+	hit_button.addEventListener("click", is_bust);
+
+}
+
+//################################################
+// is_bust - checks once if the player went over 21
+// or on 21 exactly
+
+function is_bust() {
+	let total = 0;
+	for (let i of player_cards) {
+		if (i[1] == 14) {
+			total += 11;
+		} else if (i[1] == 11 || i[1] == 12 || i[1] == 13) {
+			total += 10;
 		} else {
-			block.className = 'black';
-			block.id = `${x+1} ${y}`;
+			total += i[1];
 		}
-		board.appendChild(block);
+	}
+	console.log(total);
+	if (total > 21) {
+		console.log("you lose");
+		clearInterval(timer);
+		let disable_hit = document.getElementById("hit");
+		disable_hit.onclick = "";
+	} else if (total == 21) {
+		result = 1;
 	}
 }
 
-function set_pieces() {
-	pawns_set();
-	rooks_set();
-	knights_set();
-	bishops_set();
-	queens_set();
-	kings_set();
-}
+//################################################
+// checks if the card is an ace and sets its value
+// to either 1 or 11
 
-function pawns_set() {
-	for (let i = 1; i < 9; i++) {
-		let pawn_white = document.createElement("img");
-		pawn_white.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Chess_plt45.svg/75px-Chess_plt45.svg.png";
-		pawn_white.style.width = "100%";
-
-		let cell_white = document.getElementById(`7 ${i}`); // set the white pawns
-		cell_white.appendChild(pawn_white);
-
-		let pawn_black = document.createElement("img");
-		pawn_black.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/75px-Chess_pdt45.svg.png";
-		pawn_black.style.width = "100%";
-
-		let cell_black = document.getElementById(`2 ${i}`); // set the black pawns
-		cell_black.appendChild(pawn_black);
+function choose_ace(card) {
+	if (card[1] == 1) {
+		let choice = confirm("Press OK to select 1\nPress cancel to select 11");
+		if (choice == true) {
+			card[1] = 1;
+		} else {
+			card[1] = 14;
+		}
 	}
 }
 
-function rooks_set() {
-	for (let i = 1; i < 9; i+=7) {
-		let rook_white = document.createElement("img");
-		rook_white.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Chess_rlt45.svg/75px-Chess_rlt45.svg.png";
-		rook_white.style.width = "100%";
+//################################################
+// restart the game
 
-		let cell_white = document.getElementById(`8 ${i}`); // set the white rooks
-		cell_white.appendChild(rook_white);
+function refresh() {
+	location.reload();
+}
 
-		let rook_black = document.createElement("img");
-		rook_black.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Chess_rdt45.svg/75px-Chess_rdt45.svg.png";
-		rook_black.style.width = "100%";
+//################################################
+// checks on repeat if the user bust or got 21
 
-		let cell_black = document.getElementById(`1 ${i}`); // set the black rooks
-		cell_black.appendChild(rook_black);
+function test_win() {
+	if (result == 1) {
+		console.log("you win");
+		clearInterval(timer);
+		let disable_hit = document.getElementById("hit");
+		disable_hit.onclick = "";
+	} else {
+		result = 0;
 	}
 }
 
-function knights_set() {
-	for (let i = 2; i < 8; i+=5) {
-		let knight_white = document.createElement("img");
-		knight_white.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Chess_nlt45.svg/75px-Chess_nlt45.svg.png";
-		knight_white.style.width = "100%";
+//################################################
+// sum up the total score of the player after
+// the player presses stand
 
-		let cell_white = document.getElementById(`8 ${i}`); // set the white knights
-		cell_white.appendChild(knight_white);
-
-		let knight_black = document.createElement("img");
-		knight_black.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Chess_ndt45.svg/75px-Chess_ndt45.svg.png";
-		knight_black.style.width = "100%";
-
-		let cell_black = document.getElementById(`1 ${i}`); // set the black knights
-		cell_black.appendChild(knight_black);
+let final_total = 0;
+function stand() {
+	while (player_cards.length > 0) {
+		let add = player_cards.pop();
+		console.log(add[1]);
+		final_total += add[1];
 	}
+	let disable_hit = document.getElementById("hit");
+	disable_hit.onclick = "";
 }
 
-function bishops_set() {
-	for (let i = 3; i < 7; i+=3) {
-		let bishop_white = document.createElement("img");
-		bishop_white.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Chess_blt45.svg/75px-Chess_blt45.svg.png";
-		bishop_white.style.width = "100%";
+//################################################
+// reveals the dealers' card 1sec after the player
+// pressed stand
 
-		let cell_white = document.getElementById(`8 ${i}`); // set the white bishops
-		cell_white.appendChild(bishop_white);
-
-		let bishop_black = document.createElement("img");
-		bishop_black.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Chess_bdt45.svg/75px-Chess_bdt45.svg.png";
-		bishop_black.style.width = "100%";
-
-		let cell_black = document.getElementById(`1 ${i}`); // set the black bishops
-		cell_black.appendChild(bishop_black);
-	}
+function card_reveal() {
+	setTimeout(function() { face_down.src = dealer_cards[0][0] }, 1000);
 }
 
-function queens_set() {
-	let queen_white = document.createElement("img");
-	queen_white.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Chess_qlt45.svg/75px-Chess_qlt45.svg.png";
-	queen_white.style.width = "100%";
+//################################################
 
-	let cell_white = document.getElementById(`8 4`); // set the white queen
-	cell_white.appendChild(queen_white);
 
-	let queen_black = document.createElement("img");
-	queen_black.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Chess_qdt45.svg/75px-Chess_qdt45.svg.png";
-	queen_black.style.width = "100%";
 
-	let cell_black = document.getElementById(`1 4`); // set the black queen
-	cell_black.appendChild(queen_black);
+
+
+let deck = create_deck();
+deck = shuffle_deck(deck);
+
+// dealers' cards
+
+let dealer_cards = [];
+dealer_cards.push(deck.pop());
+let face_down = document.createElement("img");
+face_down.src = "cards/back_red.jpg";
+let dealer_side = document.getElementById("dealer");
+dealer_side.appendChild(face_down);
+dealer_cards.push(deck.pop());
+let dealer_face_up = document.createElement("img");
+dealer_face_up.src = dealer_cards[1][0];
+dealer_side.appendChild(dealer_face_up);
+
+// players' cards
+
+let player_cards = [];
+let card = deck.pop();
+choose_ace(card);
+player_cards.push(card);
+let card1 = document.createElement("img");
+card1.src = player_cards[0][0];
+let player_side = document.getElementById("player");
+player_side.appendChild(card1);
+card = deck.pop();
+choose_ace(card);
+player_cards.push(card);
+let card2 = document.createElement("img");
+card2.src = player_cards[1][0];
+player_side.appendChild(card2);
+
+// check for bust
+
+let result;
+let hit_button = document.getElementById("hit");
+hit_button.addEventListener("click", is_bust);
+is_bust();
+let timer = setInterval(test_win, 100);
+if (result == 1) {
+	console.log("you win");
+	clearInterval(timer);
+	let disable_hit = document.getElementById("hit");
+	disable_hit.onclick = "";
+} else {
+	result = 0;
 }
 
-function kings_set() {
-	let king_white = document.createElement("img");
-	king_white.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Chess_klt45.svg/75px-Chess_klt45.svg.png";
-	king_white.style.width = "100%";
+// dealers' turn
 
-	let cell_white = document.getElementById(`8 5`); // set the white king
-	cell_white.appendChild(king_white);
-
-	let king_black = document.createElement("img");
-	king_black.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Chess_kdt45.svg/75px-Chess_kdt45.svg.png";
-	king_black.style.width = "100%";
-
-	let cell_black = document.getElementById(`1 5`); // set the black king
-	cell_black.appendChild(king_black);
-}
-
-
-set_pieces();
-
-
-
-// function white_pawn_move(e) {
-// 	let this.
-// }
+let dealer_stand = document.getElementById("stand");
+dealer_stand.addEventListener("click", card_reveal);
